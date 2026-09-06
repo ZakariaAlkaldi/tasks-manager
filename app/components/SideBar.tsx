940e0514642b7265419dee276e3bc0323e25ee4d
ZakariaAlkaldi <zakariealkaldi@gmail.com>
2026-09-06 13:32:53 +0300

    Made The NavBar Responsive

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,26 +1,47 @@
 import Link from "next/link";
-import React from "react";
+import Image from "next/image";
 
 const SideBar = () => {
   return (
-    <aside className="w-[20%] h-screen flex flex-col gap-6 text-[#ffffff] p-10 bg-[#141C2B]">
-      <h2 className="text-3xl font-extrabold border-b border-[#374151] pb-6">
+    <aside className="md:w-[20%] md:p-10 w-fit h-screen flex flex-col gap-6 text-[#ffffff] pt-10 bg-[#141C2B]">
+      <h2 className="lg:text-3xl text-2xl lg:font-extrabold font-normal border-b border-[#374151] pb-6 md:block hidden">
         نظام ادارة العملاء
       </h2>
       <ul className="flex flex-col gap-3">
         <Link href={"/"}>
-          <li className="text-2xl font-light cursor-pointer hover:bg-[#374151] w-full hover:rounded-sm py-[5px] px-[10px]">
-            لوحة التحكم
+          <li className="flex gap-2 items-center lg:text-2xl text-base font-light cursor-pointer hover:bg-[#374151] w-full hover:rounded-sm py-1.25 px-2.5">
+            <Image
+              src={"/icons/dashboard.svg"}
+              alt="dashboard"
+              width={35}
+              height={35}
+              className="md:hidden block"
+            />
+            <p className="md:block hidden">لوحة التحكم</p>
           </li>
         </Link>
         <Link href={"/customers"}>
-          <li className="text-2xl font-light cursor-pointer hover:bg-[#374151] w-full hover:rounded-sm py-[5px] px-[10px]">
-            العملاء
+          <li className="flex gap-2 items-center lg:text-2xl text-base font-light cursor-pointer hover:bg-[#374151] w-full hover:rounded-sm py-1.25 px-2.5">
+            <Image
+              src={"/icons/customer.svg"}
+              alt="customers"
+              width={35}
+              height={35}
+              className="md:hidden block"
+            />
+            <p className="md:block hidden">العملاء</p>
           </li>
         </Link>
         <Link href={"/tasks"}>
-          <li className="text-2xl font-light cursor-pointer hover:bg-[#374151] w-full hover:rounded-sm py-[5px] px-[10px]">
-            المهام
+          <li className="flex gap-2 items-center lg:text-2xl text-base font-light cursor-pointer hover:bg-[#374151] w-full hover:rounded-sm py-1.25 px-2.5">
+            <Image
+              src={"/icons/task.svg"}
+              alt="tasks"
+              width={35}
+              height={35}
+              className="md:hidden block"
+            />
+            <p className="md:block hidden">المهام</p>
           </li>
         </Link>
       </ul>
